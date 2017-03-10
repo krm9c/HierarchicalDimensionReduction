2ee946222815cb9bd863f1be765426ff0eb39a3d
Krishnan Raghavan <krm9c@mst.edu>
2017-03-10 13:04:36 -0600

    this is the new file for testing paper-1 with bootstrap

---
+++ Paper_1_Mar10_17.py
@@ -0,0 +1,11 @@
+"""
+Testing For Paper-I
+"""
+import math
+import numpy as np
+import time
+## We have to now import our libraries
+from Data_import import *
+## First Let us import our two data-sets
+## First up is the Rolling Element Bearing Data-set
+print  "The data import file is all ready now"
