b047bf0f152dd4a9a2e7d8f01c1a24dfc63ed14a
Krishnan Raghavan <krm9c@mst.edu>
2017-03-29 10:50:13 -0500

    Script for installation of the packages

---
+++ Installation.sh
@@ -0,0 +1,24 @@
+pip install six numpy wheel sudo apt-get install python-scipy libblas-dev liblapack-dev gfortran
+sudo apt-get install libxslt-dev  # for libxml from scrapy
+sudo apt-get install libcupti-dev
+export BLAS=/usr/lib/libblas.so
+export LAPACK=/usr/lib/liblapack.so
+pip install numpy
+pip install scipy
+pip install scikit-learn
+pip install pandas
+pip install patsy
+pip install statsmodels
+pip install ipython tornado pyzmq
+pip install networkx
+pip install gensim
+pip install scrapy
+pip install numexpr bottleneck
+pip install sqlalchemy
+pip install nltk
+pip install seaborn
+pip install pillow
+pip install h5py
+pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+pip install keras
+pip install pymc3
